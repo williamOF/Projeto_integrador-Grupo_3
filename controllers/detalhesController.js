c486b0ad6b4c44db6d7bc43248c4edb9d8ef1535
andersongrt50 <andersongrt50@gmail.com>
2022-11-09 14:36:32 -0300

    adicionando id aos livros e mais livros

--- controllers/detalhesController.js
+++ controllers/detalhesController.js
@@ -1,7 +1,12 @@
+const fs = require('fs')
+const path = require('path')
+
+const arquivo = path.join(__dirname , "../database/data.json")
+const produtos = JSON.parse(fs.readFileSync(arquivo, "utf-8"))
 
 
 module.exports ={
     produto : (req,res) => {
-        res.render('detalhes')
+        res.render('detalhes.ejs', {produtos})
     }
 }
