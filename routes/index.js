80ba40e27ae3b47426c8c363f4fe3a640418c1bc
williamOF <maike.study@gmail.com>
2022-11-10 20:30:57 -0300

    detalhe interno de um produto

--- routes/index.js
+++ routes/index.js
@@ -2,13 +2,15 @@ var express = require('express');
 var router = express.Router();
 
 // importação dos controllers
-const indexController = require('../controllers/homeController')
+const indexController = require('../controllers')
 
 
 
 /* GET home page. */
 router.get('/', indexController.home);
 router.get('/biblioteca', indexController.biblioteca )
+router.get('/showbook/:id', indexController.showbook)
+router.get('/carrinho', indexController.carrinho)
 
 
 module.exports = router;
