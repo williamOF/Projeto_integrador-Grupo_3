0692bb71aabdb3963f3509cce819b7413e6071c4
williamOF <maike.study@gmail.com>
2022-12-13 01:34:29 -0300

    alguns bugs encontrados

--- controllers/purchasController.js
+++ controllers/purchasController.js
@@ -13,32 +13,52 @@ const {Books} = require('../database/models')
 module.exports = {
     add: async (req,res) => {
         const {id_book, type, qtd} = req.body
-
+        const cart = req.session.cart
+        
         const DataBook = await Books.findByPk(id_book)
         const book = DataBook.dataValues
-
-        console.log(type)
-
+        
        if(book.inventory > qtd){
+            let price = 0
+            let unit_price =0
+            switch (type) {
+                case 'kindle':
+                    price = book.kindle_price * parseInt(qtd)
+                    unit_price = book.kindle_price
+                    break;
+                case 'common':
+                    price = book.common_price * parseInt(qtd)
+                    unit_price = book.common_price
+                    break;
+                case 'special':
+                    price = book.special_price * parseInt(qtd)
+                    unit_price = book.special_price
+                    break;
+                case 'all-types':
+                    price = (book.special_price + book.common_price + book.kindle_price) * parseInt(qtd)
+                    unit_price = (book.special_price + book.common_price + book.kindle_price)
+                    break;
+                default:
+                    break;
+            }
+           
 
-        let price = 0
+            let inCart = {
+                id_cart: req.session.cart ? req.session.cart.length + 1 : 1 ,
+                purchase_value: price,
+                unit_price,
+                qtd_products: qtd,
+                type_selected: type,
+                id_books:book.id_books
+            } 
 
-        switch (type) {
-            case 'kindle':
-                price = book.kindle_price * parseInt(qtd)
-                break;
-            case 'common':
-                price = book.common_price * parseInt(qtd)
-                break;
-            case 'special':
-                price = book.special_price * parseInt(qtd)
-                break;
-            case 'all-types':
-                price = (book.special_price + book.common_price + book.kindle_price) * parseInt(qtd)
-                break;
-            default:
-                break;
-        }
+            if(cart == undefined){
+                req.session.cart = [inCart]
+            }else{
+                req.session.cart.push(inCart)
+            }
+
+            return res.redirect('/carrinho')
           
         }else{
 
@@ -49,72 +69,16 @@ module.exports = {
         }
 
     },
-    carrinho: (req,res) => {
-        const destaque  = produtos.filter( p => p.destaque == 1 ) 
-        const DataProducts = fsCrud.read('../database/database/data.json')
-        let cartDb = fsCrud.read('../database/database/cart.json')
-      
+    carrinho: async (req,res) => {
+        const emphasis = await Books.findAll({ where: {genre: 'fiction'}})
         let cart = req.session.cart
-        let arrayProductsDetails = []
-
-        if(cart){
-            for(book of cart) {
-                let seekBook = DataProducts.find(p => p.id == book.id_product)
-                
-                let vKindle = parseFloat(seekBook.kindle);
-                let vCommon = parseFloat(seekBook.common);
-                let vSpecial = parseFloat(seekBook.special);
-                let lastPrice = 0
-                let valUnidade = 0
-
-                switch (book.type) {
-                    case 'kindle':
-                        lastPrice = vKindle * parseInt(book.unidades)
-                        valUnidade = vKindle
-                        break;
-                    case 'common':
-                        lastPrice = vCommon * parseInt(book.unidades)
-                        valUnidade = vCommon
-                        break;
-                    case 'special':
-                        lastPrice = vSpecial * parseInt(book.unidades)
-                        valUnidade = vSpecial
-                        break;
-                    case "all-types":
-                        valUnidade = vCommon + vKindle + vSpecial
-                        lastPrice = valUnidade * parseInt(book.unidades)
-                        break;
-                    default:
-                        break;
-                }
-
-                let addCart = {
-                    id_pedido: book.id_cart,
-                    type_selected: book.type,
-                    qtd_selected: book.unidades,
-                    valUnidade,
-                    valorDoPedido:toThousand(lastPrice),
-                    product: seekBook
-                }
-
-                
-                arrayProductsDetails.push(addCart)
-                cartDb.push(addCart)
-            }
-        } 
-
-        
-        let valorTotalCompra= 0
-        for(pedido of arrayProductsDetails){
-            valorTotalCompra += parseFloat(pedido.valorDoPedido) 
-        }
-        
-
+        let admin = req.session.admin
+       
         res.render('carrinho',{
-            destaque,
-            produtos:arrayProductsDetails,
-            admin:req.session.admin,
-            price:toThousand(valorTotalCompra)
+            emphasis,
+            shopping_cart:[],
+            admin,
+            price:'toThousand(shopping_value)'
         })
     },
     remover: (req,res) =>{
@@ -123,8 +87,8 @@ module.exports = {
         if(id){
             let inCart = req.session.cart
 
-            let cartRemoved = inCart.filter(prod => prod.id_cart != id)
-            req.session.cart = cartRemoved
+            let removed = inCart.filter(prod => prod.id_cart != id)
+            req.session.cart = removed
         }
         return res.redirect('/carrinho')
     },
